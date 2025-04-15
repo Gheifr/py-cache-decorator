aa4024068a583da64a86c96d7ba69b700bd549c0
Vitalii Prushak <v.prushak@gmail.com>
2025-04-15 14:30:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args, **kwargs) -> None:
+        cache_record_key = (args, tuple(frozenset(kwargs.items())))
+        if cache_dict:
+            if cache_record_key in cache_dict:
+                print("Getting from cache")
+                return cache_dict[cache_record_key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_dict[cache_record_key] = result
+        return result
+    return inner
